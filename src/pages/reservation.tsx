a281587d3e64b7a3b834e8973abdd028b89b4a9d
eternal <eternal.git@gmail.com>
2023-10-16 03:43:09 +0100

    modify reservation page

--- src/pages/reservation.tsx
+++ src/pages/reservation.tsx
@@ -3,6 +3,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '
 import { CalendarCheck, CalendarIcon, Check, Clock, Contact, PersonStanding } from 'lucide-react';
 import { Popover, PopoverContent, PopoverTrigger } from '~/components/popover';
 import { zodResolver } from '@hookform/resolvers/zod';
+import Textarea from '~/components/textarea';
 import moment, { type Moment } from 'moment';
 import Calendar from '~/components/calendar';
 import { Page } from '~/components/layouts';
@@ -24,6 +25,8 @@ const FormSchema = z.object({
 	date: z.date(),
 	time: z.string(),
 	people: z.number().min(Information.Bookings.People.min).max(Information.Bookings.People.max),
+	message: z.string().optional(),
+	name: z.string(),
 	email: z.string().email(),
 	phone: z.string().regex(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/, {
 		get message() {
@@ -40,8 +43,7 @@ function Reservation() {
 	const form = useForm<z.infer<typeof FormSchema>>({
 		resolver: zodResolver(FormSchema),
 		defaultValues: {
-			people: Information.Bookings.People.min,
-			email: ''
+			people: Information.Bookings.People.min
 		}
 	});
 
@@ -67,17 +69,17 @@ function Reservation() {
 						{i18n.Messages.BOOKING_DETAILS}
 					</h3>
 				</div>
-				<div className='flex flex-col sm:flex-row items-start gap-5 w-full flex-wrap'>
+				<div className='flex flex-col items-start gap-5 w-full flex-wrap'>
 					<FormField
 						control={form.control}
 						name='date'
 						render={({ field }) => (
-							<FormItem className='flex flex-col w-full sm:[width:unset]'>
-								<FormLabel>{i18n.Messages.DATE}</FormLabel>
+							<FormItem className='flex flex-col w-full'>
+								<FormLabel>{i18n.Messages.DATE} *</FormLabel>
 								<Popover>
 									<PopoverTrigger asChild>
 										<FormControl>
-											<Button variant='outline' className={cn('w-full sm:w-[325px] justify-start text-left font-normal', !field.value && 'text-muted-foreground')}>
+											<Button variant='outline' className={cn('w-full justify-start text-left font-normal', !field.value && 'text-muted-foreground')}>
 												<CalendarIcon className='mr-2 h-4 w-4' />
 												<span className='select-none'>
 													{field.value ? formatter.format(field.value) : i18n.Messages.PICK_RESERVATION_DATE}
@@ -144,11 +146,11 @@ function Reservation() {
 							const available = times && intervals(start as Moment, end as Moment);
 
 							return (
-								<FormItem className='flex flex-col w-full sm:[width:unset]'>
-									<FormLabel>{i18n.Messages.TIME}</FormLabel>
+								<FormItem className='flex flex-col w-full '>
+									<FormLabel>{i18n.Messages.TIME} *</FormLabel>
 									<Select onValueChange={field.onChange} value={field.value} disabled={!times}>
 										<FormControl>
-											<SelectTrigger className='w-full sm:w-[325px] px-4'>
+											<SelectTrigger className='w-full  px-4'>
 												<div className='flex items-center gap-2'>
 													<Clock className='h-4 w-4' />
 													<SelectValue
@@ -176,7 +178,7 @@ function Reservation() {
 						control={form.control}
 						name='people'
 						render={({ field }) => (
-							<FormItem className='flex flex-col w-full sm:[width:unset]'>
+							<FormItem className='flex flex-col w-full '>
 								<FormLabel>{i18n.Messages.PEOPLE}</FormLabel>
 								<FormControl>
 									<Select
@@ -184,7 +186,7 @@ function Reservation() {
 										onValueChange={v => field.onChange(Number(v))}
 										value={field.value?.toString()}
 									>
-										<SelectTrigger className='w-full sm:w-[325px] px-4'>
+										<SelectTrigger className='w-full  px-4'>
 											<div className='flex items-center gap-2'>
 												<PersonStanding className='h-4 w-4' />
 												<SelectValue
@@ -217,15 +219,28 @@ function Reservation() {
 						{i18n.Messages.CONTACT_DETAILS}
 					</h3>
 				</div>
-				<div className='flex flex-col sm:flex-row items-start gap-5 w-full mb-6'>
+				<div className='flex flex-col items-start gap-5 w-full mb-4 flex-wrap'>
+					<FormField
+						control={form.control}
+						name='name'
+						render={({ field }) => (
+							<FormItem className='flex flex-col w-full '>
+								<FormLabel>{i18n.Messages.NAME} *</FormLabel>
+								<FormControl>
+									<Input className='w-full' placeholder={i18n.Messages.RESERVATION_PLACEHOLDER_NAME} {...field} value={field.value ?? ''} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 					<FormField
 						control={form.control}
 						name='email'
 						render={({ field }) => (
-							<FormItem className='flex flex-col w-full sm:[width:unset]'>
-								<FormLabel>{i18n.Messages.EMAIL}</FormLabel>
+							<FormItem className='flex flex-col w-full '>
+								<FormLabel>{i18n.Messages.EMAIL} *</FormLabel>
 								<FormControl>
-									<Input className='w-full sm:[width:325px]' placeholder='example@gmail.com' {...field} />
+									<Input className='w-full' placeholder={i18n.Messages.RESERVATION_PLACEHOLDER_EMAIL} {...field} value={field.value ?? ''} />
 								</FormControl>
 								<FormMessage />
 							</FormItem>
@@ -235,17 +250,32 @@ function Reservation() {
 						control={form.control}
 						name='phone'
 						render={({ field }) => (
-							<FormItem className='flex flex-col w-full sm:[width:unset]'>
-								<FormLabel>{i18n.Messages.PHONE_NUMBER}</FormLabel>
+							<FormItem className='flex flex-col w-full '>
+								<FormLabel>{i18n.Messages.PHONE_NUMBER} *</FormLabel>
 								<FormControl>
-									<Input className='w-full sm:[width:325px]' placeholder='+33 123456789' {...field} />
+									<Input className='w-full' placeholder={i18n.Messages.RESERVATION_PLACEHOLDER_PHONE_NUMBER} {...field} value={field.value ?? ''} />
 								</FormControl>
 								<FormMessage />
 							</FormItem>
 						)}
 					/>
 				</div>
-				<Button className='w-full sm:[width:unset]' type='submit'>
+				<div className='flex flex-col items-start gap-5 w-full mb-6 flex-wrap'>
+					<FormField
+						control={form.control}
+						name='message'
+						render={({ field }) => (
+							<FormItem className='flex flex-col w-full'>
+								<FormLabel>{i18n.Messages.MESSAGE}</FormLabel>
+								<FormControl>
+									<Textarea className='w-full' {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<Button className='w-full ' type='submit'>
 					{i18n.Messages.REQUEST_RESERVATION}
 				</Button>
 			</form>
